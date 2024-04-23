d622f005aa363be00f9bf6f9807f5009f8de2f1c
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-23 16:38:52 +0200

    setup mongoose

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const PORT = 5005;
@@ -8,41 +9,51 @@ const PORT = 5005;
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-app.use(cors({
-  origin: function (origin, callback) {
-    // Regex to check if the origin is from http://localhost with port from 5100 to 5299
-    const portRegex = /^http:\/\/localhost:(517\d|518\d)$/;
-    if (!origin || portRegex.test(origin)) {
-      callback(null, true);  // Origin is allowed
-    } else {
-      callback(new Error('Not allowed by CORS'));  // Origin is not allowed
-    }
-  }
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Regex to check if the origin is from http://localhost with port from 5100 to 5299
+      const portRegex = /^http:\/\/localhost:(517\d|518\d)$/;
+      if (!origin || portRegex.test(origin)) {
+        callback(null, true); // Origin is allowed
+      } else {
+        callback(new Error("Not allowed by CORS")); // Origin is not allowed
+      }
+    },
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
+
+//Middleware
+const MONGODB_URI = "mongodb://127.0.0.1:27017/cohort-tools-project";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+    app.listen(PORT, () => {
+      console.log(`Server listening on port https://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("Error connecting to mongo", err));
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-
 // START SERVER
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
